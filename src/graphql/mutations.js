6725015234a5f8344ec6f2b79c39c69d103f9b7c
Ross Sickora <r.sickora@gmail.com>
2019-10-02 14:55:20 -0500

    missed some amplify stuff

---
+++ src/graphql/mutations.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createResponse = `mutation CreateResponse($value: String!, $user: String) {
+  createResponse(value: $value, user: $user) {
+    id
+    value
+    user
+  }
+}
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getAllResponses = `query GetAllResponses {
+  getAllResponses {
+    id
+    value
+    user
+  }
+}
+`;
+export const getResponse = `query GetResponse($id: ID!) {
+  getResponse(id: $id) {
+    id
+    value
+    user
+  }
+}
+`;
